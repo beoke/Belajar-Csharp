1bd3b9ece38b7dd3b95e5015985c5a584e0ea20e
buble <sucsespart1@gmail.com>
2024-08-15 12:30:48 +0700

    Add files via upload

---
+++ Latih2_TipeDataJamak/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Latih2_TipeDataJamak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> angkalist = new List<int> { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Elemen pertama: " + angkalist[0]);
+            Console.WriteLine("Element terakhir: " + angkalist[angkalist.Count -1]);
+
+            angkalist.Add(6);
+            Console.WriteLine("Element baru ditambahkan: " + angkalist[5]);
+
+            angkalist.RemoveAt(2);
+            Console.WriteLine("List setelah elemen ketiga dihapus: " + string.Join(", ", angkalist));
+        }
+    }
+}

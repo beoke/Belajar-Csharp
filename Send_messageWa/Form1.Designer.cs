f9af8de760ba71b116a112d5fe2acde1269ad0b7
buble <sucsespart1@gmail.com>
2024-08-18 22:11:17 +0700

    Add files via upload

---
+++ Send_messageWa/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Send_messageWa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tx_number = new System.Windows.Forms.TextBox();
+            this.tx_message = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message";
+            // 
+            // tx_number
+            // 
+            this.tx_number.Location = new System.Drawing.Point(105, 27);
+            this.tx_number.Name = "tx_number";
+            this.tx_number.Size = new System.Drawing.Size(162, 20);
+            this.tx_number.TabIndex = 2;
+            // 
+            // tx_message
+            // 
+            this.tx_message.Location = new System.Drawing.Point(105, 53);
+            this.tx_message.Multiline = true;
+            this.tx_message.Name = "tx_message";
+            this.tx_message.Size = new System.Drawing.Size(162, 55);
+            this.tx_message.TabIndex = 3;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(192, 126);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 4;
+            this.btn_send.Text = "Send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 225);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.tx_message);
+            this.Controls.Add(this.tx_number);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tx_number;
+        private System.Windows.Forms.TextBox tx_message;
+        private System.Windows.Forms.Button btn_send;
+    }
+}
+
---
+++ Send_messageWa/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Send_messageWa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void sendWhatsApp(string number, string message)
+        {
+            try
+            {
+                if (number == "")
+                {
+                    MessageBox.Show("No number added");
+                }
+                if (number.Length <= 0)
+                {
+                    number = "+62" + number;
+                }
+                number = number.Replace(" ","");
+                System.Diagnostics.Process.Start("http://api.whatsapp.com/send?phone=" +number + "&text=" +message);
+            }
+            catch (Exception Ex)
+            {
+
+                MessageBox.Show(Ex.Message);
+            }
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            sendWhatsApp(tx_message.Text, tx_message.Text);
+            cleartextbox();
+        }
+        public void cleartextbox()
+        {
+            tx_message.Text = "";
+            tx_number.Text = "";
+           
+        }
+    }
+}
